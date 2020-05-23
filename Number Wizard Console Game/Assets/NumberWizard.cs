087be67d8cb774fc5b1c9720d743b48e60092cde
Ketan <ketantoo@gmail.com>
2020-05-23 10:35:14 +0530

    Restart functionality added

--- Number Wizard Console Game/Assets/NumberWizard.cs
+++ Number Wizard Console Game/Assets/NumberWizard.cs
@@ -7,30 +7,33 @@ public class NumberWizard : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
+        
+    }
+
+    void StartGame() {
         max = 1000;
         min = 1;
         guess = max;
 
         Debug.Log("Welcome to number wizard, yo");
         Debug.Log("Pick a number");
-        Debug.Log("Highest number is: "+max);
-        Debug.Log("Lowest number is: "+min);
-        Debug.Log("Tell me if your number higher than: "+ guess);
+        Debug.Log("Highest number is: " + max);
+        Debug.Log("Lowest number is: " + min);
+        Debug.Log("Tell me if your number higher than: " + guess);
         Debug.Log("Push up: Higher, Push down: Lower, Push Enter: Correct ");
         max += 1;
-
     }
 
-    
-	
-	// Update is called once per frame
-	void Update () {
+
+
+    // Update is called once per frame
+    void Update () {
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
             Debug.Log("UP Arrow was clicked.");
             min = guess;
             guess = (guess + max) / 2;
             tries++;
-            Debug.Log("Is your number :"+guess +"?");
+            Debug.Log("Is your number :" + guess + "?");
 
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow)) {
@@ -42,7 +45,10 @@ public class NumberWizard : MonoBehaviour {
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Guessed in "+ tries +" tries");
+            Debug.Log("Guessed in " + tries + " tries" + "\n Press R to start again.");
+        } else if (Input.GetKeyDown(KeyCode.R)) {
+            StartGame();
+            Debug.Log("Game Restarted.");
         }
     }
 }
