37a7dd1675abb498f7f18dfa8f6e5972f4574754
Ketan <ketantoo@gmail.com>
2020-05-23 08:54:21 +0530

    initial commit

---
+++ Number Wizard Console Game/Assets/NumberWizard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int max =1000, min= 1;
+
+        Debug.Log("Welcome to number wizard, yo");
+        Debug.Log("Pick a number");
+        Debug.Log("Highest number is: "+max);
+        Debug.Log("Lowest number is: "+min);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
